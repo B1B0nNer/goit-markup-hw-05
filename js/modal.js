c004160d139e6fb2ded541b7dcad875af272cafb
Nikita <barbos.nik007@gmail.com>
2023-02-17 14:09:24 +0100

    qwe

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      console.log(123);
-    }
-  })();
+  const refs = {
+    openModalBtn: document.getElementsByClassName("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    console.log("123");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
